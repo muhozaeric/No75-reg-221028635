4b48b7182c318898f64f201056314ca09aeeeecf
muhozaeric <149691487+muhozaeric@users.noreply.github.com>
2023-11-05 03:26:46 -0800

    Add files via upload

---
+++ increments.java
@@ -0,0 +1,9 @@
+//Java Program to demonstrate the example of for loop which shows increment
+public class increments {
+    public static void main(String[]args){
+        //initialization 
+       for(int age=8;age<65;age++){
+        System.out.println(age);//print output
+       }
+}
+}
